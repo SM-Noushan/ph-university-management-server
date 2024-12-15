d86440d5bb8215704078d846e822fbe9712d9d89
SMNoushan <noushan.md@gmail.com>
2024-12-15 19:20:13 +0600

    semester registration interface

---
+++ src/app/modules/semesterRegistration/semesterRegistration.interface.ts
@@ -0,0 +1,12 @@
+import { Types } from "mongoose";
+
+export type TSemesterRegistrationStatus = "UPCOMING" | "ONGOING" | "ENDED";
+
+export type TSemesterRegistration = {
+  academicSemester: Types.ObjectId;
+  status: TSemesterRegistrationStatus;
+  minCredit: number;
+  maxCredit: number;
+  start: Date;
+  end: Date;
+};
