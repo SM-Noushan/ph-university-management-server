95923a9bfd77a0a67d4361bf9f6b1af86e36fdf4
SMNoushan <noushan.md@gmail.com>
2024-12-16 13:22:10 +0600

    added: offeredCourses model, interface

---
+++ src/app/modules/offeredCourses/offeredCourses.constant.ts
@@ -0,0 +1,11 @@
+import { TDay } from "./offeredCourses.interface";
+
+export const DaysEnum: TDay[] = [
+  "Sat",
+  "Sun",
+  "Mon",
+  "Tue",
+  "Wed",
+  "Thu",
+  "Fri",
+];
---
+++ src/app/modules/offeredCourses/offeredCourses.model.ts
@@ -0,0 +1,64 @@
+import { model, Schema } from "mongoose";
+import { DaysEnum } from "./offeredCourses.constant";
+import { TOfferedCourses } from "./offeredCourses.interface";
+
+const offeredCoursesSchema = new Schema<TOfferedCourses>(
+  {
+    semesterRegistration: {
+      type: Schema.Types.ObjectId,
+      ref: "SemesterRegistration",
+      required: true,
+    },
+    academicSemester: {
+      type: Schema.Types.ObjectId,
+      ref: "AcademicSemester",
+      required: true,
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: "AcademicFaculty",
+      required: true,
+    },
+    academicDepartment: {
+      type: Schema.Types.ObjectId,
+      ref: "AcademicDepartment",
+      required: true,
+    },
+    course: {
+      type: Schema.Types.ObjectId,
+      ref: "Course",
+      required: true,
+    },
+    faculty: {
+      type: Schema.Types.ObjectId,
+      ref: "Faculty",
+      required: true,
+    },
+    maxCapacity: {
+      type: Number,
+      required: true,
+    },
+    section: {
+      type: String,
+      required: true,
+    },
+    days: {
+      type: String,
+      enum: DaysEnum,
+      required: true,
+    },
+    startTime: {
+      type: String,
+      required: true,
+    },
+    endTime: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export OfferedCourses = model<TOfferedCourses>("OfferedCourses", offeredCoursesSchema);
