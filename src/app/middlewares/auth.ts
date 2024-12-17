0451c3e40dbb029bfc08cfc9f61c6d99b2b2c349
SMNoushan <noushan.md@gmail.com>
2024-12-17 20:03:24 +0600

    updated: auth middleware => validate token

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,13 +1,23 @@
+import config from "../config";
 import status from "http-status";
 import AppError from "../errors/AppError";
 import catchAsync from "../utils/catchAsync";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { NextFunction, Request, Response } from "express";
 
 const auth = () =>
   catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization;
+
+    // if token is present
     if (!token) throw new AppError(status.UNAUTHORIZED, "Unauthorized access");
-    return next();
+
+    // if token is valid
+    jwt.verify(token, config.JwtAccessSecret as string, (err, decoded) => {
+      if (err) throw new AppError(status.UNAUTHORIZED, "Unauthorized access");
+      req.user = decoded as JwtPayload;
+      next();
+    });
   });
 
 export default auth;
